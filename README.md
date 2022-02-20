038bbe3598aae852ed3bc790509184f0bc55f437
AniketYadav17 <anikety0000@gmail.com>
2022-02-21 00:37:26 +0530

    updated

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-# Data-Summarization
 
-Deployed:- https://arshdeep.pythonanywhere.com/
+# Abstractive Text Summarization
 
-"app.py" is the code for data summarization. For using this code please clone this repo. as the structuring/documentation is very important. 
+Abstractive Text Summarization is the task of generating a short and concise summary that captures the salient ideas of the source text. The generated summaries potentially contain new phrases and sentences that may not appear in the source text.
+
+## Installation
+
+1. Install the zip file from the Code drop-down
+
+2. Clone the repository.
+
+Create a virtual environment and install the dependencies.
+
+```bash
+  pip install -r requirements.txt
+```
+    
+## Demo
+
+https://github.com/AniketYadav17/abstractive-text-summarizer
+
+
+## Screenshots
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import requests
+import os
+from wsgiref import simple_server
 from flask import Flask,render_template,url_for
+from flask_cors import CORS, cross_origin
 from flask import request as req
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
 
 
 app = Flask(__name__)
+CORS(app)
+
 @app.route("/",methods=["GET","POST"])
+@cross_origin()
 def Index():
     return render_template("index.html")
 
 @app.route("/Summarize",methods=["GET","POST"])
+@cross_origin()
 def Summarize():
     if req.method== "POST":
         API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
@@ -30,3 +40,12 @@ def Summarize():
         return render_template("index.html",result=output["summary_text"])
     else:
         return render_template("index.html")
+
+
+port = int(os.getenv("PORT", 5000))
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    # port = 5000
+    httpd = simple_server.make_server(host, port, app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
